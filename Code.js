2bb8973899963266b44fbc3bff0d0eadd73a2ea5
dranidis <dranidis@gmail.com>
2020-05-18 20:06:14 +0300

    Add fahr2celcius

--- Code.js
+++ Code.js
@@ -1,15 +1,19 @@
 function quadraticSolution(a, b, c, which) {
   discriminant = b * b - 4 * a * c;
-  
+
   if (discriminant > 0) {
     sqd = Math.sqrt(discriminant)
-    
-    if (which == 0) 
+
+    if (which == 0)
       return (-b + sqd) / (2 * a)
     else
       return (-b - sqd) / (2 * a)
-  } 
+  }
   else {
     return NaN;
-  }  
+  }
+}
+
+function fahrenheitToCelcius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
 }
