fbefb86587582f07cba2c5d2c26e305f52df31a1
dranidis <dranidis@gmail.com>
2020-05-18 19:47:50 +0300

    1st commit

---
+++ Code.js
@@ -0,0 +1,15 @@
+function quadraticSolution(a, b, c, which) {
+  discriminant = b * b - 4 * a * c;
+  
+  if (discriminant > 0) {
+    sqd = Math.sqrt(discriminant)
+    
+    if (which == 0) 
+      return (-b + sqd) / (2 * a)
+    else
+      return (-b - sqd) / (2 * a)
+  } 
+  else {
+    return NaN;
+  }  
+}
